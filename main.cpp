5f44f9c29ddd93e33d5ee60b568e0b487167c690
Paul Martin <pmartin@casestack.com>
2016-08-01 08:31:05 -0500

    Adding articulation points to the solution

--- main.cpp
+++ main.cpp
@@ -15,25 +15,30 @@ int main(int argc, char **argv)
         .add(7,6).add(7,8)
         .add(8,6).add(8,7);
 
-    vector<csce::Graph> solution;
+    vector<csce::Graph> biconnectedComponents;
     
     csce::Graph comp1;
     comp1.add(1,2).add(2,3).add(3,1);
-    solution.push_back(comp1);
+    biconnectedComponents.push_back(comp1);
     
     csce::Graph comp2;
     comp2.add(1,4).add(4,5).add(5,0).add(0,1);
-    solution.push_back(comp2);
+    biconnectedComponents.push_back(comp2);
 
     csce::Graph comp3;
     comp3.add(0,6);
-    solution.push_back(comp3);
+    biconnectedComponents.push_back(comp3);
 
     csce::Graph comp4;
     comp4.add(6,8).add(8,7).add(7,6);
-    solution.push_back(comp4);
+    biconnectedComponents.push_back(comp4);
 
-    // run your algorithm and test it against the solution above
+    vector<size_t> articulationPoins;
+    articulationPoins.push_back(0);
+    articulationPoins.push_back(1);
+    articulationPoins.push_back(6);
+    
+    // run your algorithm and test it against the biconnectedComponents above
     // your result should be the exact same number of components
     // you can just use the "==" operator to check if two Graph components are the same
 
