c01a780777fbfcadc432631eb6e501cce5373afa
Amin Rois Sinung Nugroho <sinunk1405@gmail.com>
2016-08-03 11:42:12 -0500

    Added Alex's Graph Generator.

--- main.cpp
+++ main.cpp
@@ -1,46 +1,138 @@
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+#include <vector>
+#include <string>
+#include <thread>
+#include <unordered_set>
+#include <set>
+#include <cmath>
+#include <algorithm>
+#include <sstream>
+#include <utility>
+
+#include "utility.hpp"
+#include "test.hpp"
 #include "graph.hpp"
+#include "connected_components.hpp"
 
-using namespace std;
-
-int main(int argc, char **argv)
-{
-    csce::Graph testGraph;
-    testGraph.add(0,1).add(0,5).add(0,6)
-        .add(1,2).add(1,3).add(1,4).add(1,0)
-        .add(2,3).add(2,1)
-        .add(3,2).add(3,1)
-        .add(4,1).add(4,5)
-        .add(5,4).add(5,0)
-        .add(6,0).add(6,8).add(6,7)
-        .add(7,6).add(7,8)
-        .add(8,6).add(8,7);
-
-    vector<csce::Graph> biconnectedComponents;
-    
-    csce::Graph comp1;
-    comp1.add(1,2).add(2,3).add(3,1);
-    biconnectedComponents.push_back(comp1);
-    
-    csce::Graph comp2;
-    comp2.add(1,4).add(4,5).add(5,0).add(0,1);
-    biconnectedComponents.push_back(comp2);
-
-    csce::Graph comp3;
-    comp3.add(0,6);
-    biconnectedComponents.push_back(comp3);
-
-    csce::Graph comp4;
-    comp4.add(6,8).add(8,7).add(7,6);
-    biconnectedComponents.push_back(comp4);
-
-    vector<size_t> articulationPoins;
-    articulationPoins.push_back(0);
-    articulationPoins.push_back(1);
-    articulationPoins.push_back(6);
-    
-    // run your algorithm and test it against the biconnectedComponents above
-    // your result should be the exact same number of components
-    // you can just use the "==" operator to check if two Graph components are the same
-
+int main(int argc, char* argv[]) {
+	int n = 320; //the default number of vertices. This can be changed by specifying the -n runtime argument.
+	int nedges = 0;
+	int thread_count = std::thread::hardware_concurrency(); //the default number of threads is the number of hardware cores on the current computer. This can be changed by the -t runtime argument.
+	std::string input_file_path; //where to load data from, if anywhere. If this is not specified, the data will be generated at runtime.
+	std::string output_file_path; //where to write out the generated data, if anywhere.
+	long long int duration = 0;
+	bool debug = false;
+	bool test_mode = false;
+	
+	csce::graph graph;
+	
+	int c;
+	while((c = getopt(argc, argv, ":dDf:m:n:o:t:")) != -1){
+		switch(c){
+			case 'd':
+				debug = true;
+				break;
+				
+			case 'D':
+				test_mode = true;
+				break;
+				
+			case 'f':
+				if(optarg != NULL){
+					input_file_path = std::string(optarg);
+				}
+				break;
+				
+			case 'm':
+				if(optarg != NULL){
+					std::stringstream argument_stream(optarg);
+					argument_stream >> nedges;
+				}
+				break;
+				
+			case 'n':
+				if(optarg != NULL){
+					std::stringstream argument_stream(optarg);
+					argument_stream >> n;
+				}
+				break;
+				
+			case 'o':
+				if(optarg != NULL){
+					output_file_path = std::string(optarg);
+				}
+				break;
+				
+			case 't':
+				if(optarg != NULL){
+					std::stringstream argument_stream(optarg);
+					argument_stream >> thread_count;
+				}
+				break;
+				
+			case '?':
+				break;
+		}
+	}
+	
+	if(nedges == 0){
+		nedges = 2 * n;
+	}
+	
+	//
+	// run unit tests
+	//
+	if(test_mode){
+		csce::test(debug).run();
+	}
+	
+	
+	//
+	//first - load the values into the array, either by populating it
+	//        with random values or by reading the values from a file.
+	//
+	if(input_file_path.empty()){
+		//no input file was specified, so populate the array with random numbers
+		std::cout << "Populating graph with " << n << " vertices and " << nedges << " edges ... " << std::flush;
+		graph = csce::utility::generate_random(n, nedges);
+	} else {
+		//load from the specified file
+		std::cout << "Populating graph with data from the file (" << input_file_path << ") ... " << std::flush;
+		graph = csce::utility::load_from_file(input_file_path);
+	}
+	std::cout << "done." << std::endl;
+	
+	
+	//
+	//second - if the output file path was specified, write the array to the output file
+	//         so it can be used in subsequent runs of this process.
+	//
+	if(!output_file_path.empty()){
+		std::cout << "Writing graph to file (" << output_file_path << ") ... " << std::flush;
+		csce::utility::write_to_file(graph, output_file_path);
+		std::cout << "done." << std::endl;
+	}
+	
+	csce::connected_components algorithm(thread_count);
+	
+	std::cout << "-------------------------------------------" << std::endl;
+	std::cout << "Computing articulation vertices ... " << std::flush;
+	
+	std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+	std::vector<csce::vertex> result = algorithm.find_articulation_vertices(graph);
+	std::chrono::high_resolution_clock::time_point stop_time = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time).count();
+	
+	std::cout << "done in " << csce::utility::duration_string(duration) << std::endl;
+	
+	std::cout << "There were " << result.size() << " articulation vertices. " << std::flush;
+	std::cout << "-------------------------------------------" << std::endl;
+	for(csce::vertex& v : result){
+		std::cout << v << std::endl;
+	}
+	std::cout << "-------------------------------------------" << std::endl;
+	
 	return 0;
 }
