ae4819f5445a8c04c2148a83c14593bdaf74173a
Amin Rois Sinung Nugroho <sinunk1405@gmail.com>
2016-08-08 19:29:28 -0500

    Updating the paper algorithm.

--- the-paper-algo/main.cpp
+++ the-paper-algo/main.cpp
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <sstream>
 #include <utility>
+#include <unistd.h>
 
 #include "graph.hpp"
 #include "bicc.hpp"
@@ -80,22 +81,47 @@ int main(int argc, char **argv)
 	
 	long long int duration = 0;
 	
-	int nthreads = std::thread::hardware_concurrency();
+	//int nthreads = std::thread::hardware_concurrency();
+	
+	int nthreads = 4; //the default number of threads. This can be changed by the -t runtime argument.
+	std::string input_file_path; //where to load data from, if anywhere. If this is not specified, the data will be generated at runtime.
 	csce::Bicc bicc(nthreads);
 	
+	int c;
+	while((c = getopt(argc, argv, "f:t:")) != -1){
+		switch(c){
+			case 'f':
+				if(optarg != NULL){
+					input_file_path = std::string(optarg);
+				}
+				break;
+				
+			case 't':
+				if(optarg != NULL){
+					nthreads = std::stoi(optarg);
+				}
+				break;
+		}
+	}
+	
 	std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+	
+	//vector<csce::Vertex> outputArtPoints = bicc.getArticulationPoints(testGraph);
+	
 	//run the algorithm on the input, timing
-	vector<csce::Vertex> outputArtPoints = bicc.getArticulationPoints(testGraph);
+	csce::Graph inputGraph = load_from_file (input_file_path);
+	vector<csce::Vertex> outputArticulationPoints = bicc.getArticulationPoints(inputGraph);
+	
 	std::cout << "-------------------------------------------" << std::endl;
 	std::cout << "Computing articulation vertices ... " << std::flush;
 	std::chrono::high_resolution_clock::time_point stop_time = std::chrono::high_resolution_clock::now();
 	duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time).count();
 	
 	std::cout << "done in " << duration_string(duration) << std::endl;
-	
 	std::cout << "There were " << outputArtPoints.size() << " articulation vertices. " << std::endl;
 	std::cout << "-------------------------------------------" << std::endl;
 	
+	/*
 	for(csce::Vertex& v : outputArtPoints){
 		std::cout << v.str() << std::endl;
 	}
@@ -104,9 +130,8 @@ int main(int argc, char **argv)
 	if(outputArtPoints == testArtPoints){
 		std::cout << "Correct" << std::endl;
 		}
-		
 		else
 			std::cout << "Incorrect" << std::endl;
-	
+	*/
 	return 0;
 }
